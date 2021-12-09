fd4508ab07933b2667dc87941ff498898e3ff8e8
gusta <lgustavocms@gmail.com>
2021-12-09 19:20:22 -0300

    Update menu.go
    interface

--- interface/menu.go
+++ interface/menu.go
@@ -2,10 +2,21 @@ package inter
 
 import (
 	"fmt"
+	"time"
 )
 
 func Menu() {
-	var opt int
+	var opt int;
+	nomePessoa string;
+	enderecoPessoa string
+	rgCliente string
+	dataCliente time.Time
+	codigoProduto string
+	nomeProduto string
+	valorProduto float32
+	numeroVenda int
+	dataVenda time.Time
+	
 
 	fmt.Println("Seja bem vindo!")
 	fmt.Println("Escolha uma opção:")
@@ -18,5 +29,21 @@ func Menu() {
 
 	switch opt {
 	case 1:
+		fmt.Println("Ola", opt)
+		fmt.Println("Digite o nome:")
+		fmt.Println("Digite o endereço:")
+		fmt.Println("Digite o RG:")
+		fmt.Println("Digite a data de nascimento:")
+	case 2:
+		fmt.Println("Ola", opt)
+		fmt.Println("Digite o código do produto:")
+		fmt.Println("Digite o nome do produto:")
+		fmt.Println("Digite o valor do produto:")
+	case 3:
+		fmt.Println("Ola", opt)
+		fmt.Println("Digite o número da Venda:")
+		fmt.Println("Insira a data da venda:")
+		fmt.Println("Insira o cliente:")
+
 	}
 }
