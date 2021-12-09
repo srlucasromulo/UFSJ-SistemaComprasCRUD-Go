3fc3f491f567ac6b766374f6dff70ccc85909c0c
gusta <lgustavocms@gmail.com>
2021-12-09 19:59:13 -0300

    Update menu.go
    interface2

--- interface/menu.go
+++ interface/menu.go
@@ -2,21 +2,23 @@ package inter
 
 import (
 	"fmt"
-	"time"
 )
 
 func Menu() {
-	var opt int;
-	nomePessoa string;
-	enderecoPessoa string
-	rgCliente string
-	dataCliente time.Time
-	codigoProduto string
-	nomeProduto string
-	valorProduto float32
-	numeroVenda int
-	dataVenda time.Time
-	
+	var opt int
+	var nomePessoa string
+	var enderecoPessoa string
+	var rgCliente string
+	var diaDataCliente int
+	var mesDataCliente int
+	var anoDataCliente int
+	var codigoProduto int
+	var nomeProduto string
+	var valorProduto float32
+	var numeroVenda int
+	var diaDataVenda int
+	var mesDataVenda int
+	var anoDataVenda int
 
 	fmt.Println("Seja bem vindo!")
 	fmt.Println("Escolha uma opção:")
@@ -31,19 +33,27 @@ func Menu() {
 	case 1:
 		fmt.Println("Ola", opt)
 		fmt.Println("Digite o nome:")
+		fmt.Scanf("%s", nomePessoa)
 		fmt.Println("Digite o endereço:")
+		fmt.Scanf("%s", enderecoPessoa)
 		fmt.Println("Digite o RG:")
+		fmt.Scanf("%s", rgCliente)
 		fmt.Println("Digite a data de nascimento:")
+		fmt.Scanf("%d-%d-%d", &diaDataCliente, &mesDataCliente, &anoDataCliente)
 	case 2:
 		fmt.Println("Ola", opt)
 		fmt.Println("Digite o código do produto:")
+		fmt.Scanf("%d", codigoProduto)
 		fmt.Println("Digite o nome do produto:")
+		fmt.Scanf("%s", nomeProduto)
 		fmt.Println("Digite o valor do produto:")
+		fmt.Scanf("%f", &valorProduto)
 	case 3:
 		fmt.Println("Ola", opt)
 		fmt.Println("Digite o número da Venda:")
+		fmt.Scanf("%d", &numeroVenda)
 		fmt.Println("Insira a data da venda:")
-		fmt.Println("Insira o cliente:")
-
+		fmt.Scanf("%d-%d-%d", &diaDataVenda, &mesDataVenda, &anoDataVenda)
+		fmt.Println("O cliente é:", nomePessoa) //listar clientes
 	}
 }
